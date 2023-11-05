a4630546c1ab1a5322d84a4445a06b3d1d80eaf1
kotara <youremail@yourdomain.com>
2023-11-05 21:59:11 +0300

    del libre

--- sketch2.ino
+++ sketch2.ino
@@ -1,29 +1,26 @@
-#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DS1302.h> 
 #include <dht.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);                                    // SDA and SLA        
-DS1302 rtc(8, 7, 6);                                                 // Init the DS1302
-dht DHT;                                                             // Переманная
-#define DHT11_PIN 5                                                  // Переменная 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+DS1302 rtc(8, 7, 6);
+dht DHT;
+#define DHT11_PIN 5
 
-
-void setup () 
+void setup()
 {
-    rtc.halt(false); 
-    lcd.init();                                                      //  Инициируем работу с LCD дисплеем
-    lcd.backlight();                                                 //  Включаем подсветку LCD дисплея
-    
-}  
+  rtc.halt(false);
+  lcd.init();
+  lcd.backlight();
+}
 
-void loop () 
+void loop()
 {
   lcd.setCursor(0, 0);
   lcd.print(rtc.getDateStr());
-  lcd.print(" ");    
-  lcd.print(rtc.getTimeStr());                       
-  delay (5000);
+  lcd.print(" ");
+  lcd.print(rtc.getTimeStr());
+  delay(5000);
   lcd.setCursor(0, 1);
   int chk = DHT.read11(DHT11_PIN);
   lcd.print(DHT.temperature);
@@ -31,5 +28,5 @@ void loop ()
   lcd.print("C  ");
   lcd.print(DHT.humidity);
   lcd.print("%");
-  delay(1000);
-}               
+  delay(5000);
+}
