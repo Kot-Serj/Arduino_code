8304857d04472fc832308483525adfeb96cfdbf5
kotara <youremail@yourdomain.com>
2023-11-05 20:29:47 +0300

    new_commit(printTemp)

--- sketch2.ino
+++ sketch2.ino
@@ -1,17 +1,13 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DS1302.h> 
-#include <dht.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);                                    // SDA and SLA        
 DS1302 rtc(8, 7, 6); // Init the DS1302
-dht DHT;
 
-#define DHT11_PIN 5
 
 void setup () 
 {
-    dht.begin();
     rtc.halt(false); 
     lcd.init();                                                      //  Инициируем работу с LCD дисплеем
     lcd.backlight();                                                 //  Включаем подсветку LCD дисплея
@@ -20,7 +16,6 @@ void setup ()
 
 void loop () 
 {
-  
   lcd.setCursor(0, 0);
   lcd.print(rtc.getDateStr());
   lcd.print(" ");    
